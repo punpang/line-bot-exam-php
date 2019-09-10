6976ac4f9f7d688423921e56d115fd000a969887
punpang <pansit.me@gmail.com>
2019-09-10 12:50:07 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -30,7 +30,8 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => 'ทดสอบ LINE BOT',
+				//[$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
